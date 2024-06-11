9d0c0dede3874201c12fb0613e27297d63fe824c
Guido Salazar Vargas <Guido.salazar.vargas7@gmail.com>
2024-06-11 15:33:01 -0400

    Funcional

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { PaymentsModule } from './payments/payments.module';
+
+
+
+@Module({
+  imports: [PaymentsModule],
+
+})
+export class AppModule {}
