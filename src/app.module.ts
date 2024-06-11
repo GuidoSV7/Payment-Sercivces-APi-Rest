688d1bbe021041af00d7aa7e5e0a8bef58319c36
Guido Salazar Vargas <Guido.salazar.vargas7@gmail.com>
2024-06-11 18:58:27 -0400

    Probando deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,7 @@ import { PaymentsModule } from './payments/payments.module';
   imports: [
     PaymentsModule,
 
-    ConfigModule.forRoot({isGlobal:true}),
-
-    TypeOrmModule.forRoot({
-      ssl: process.env.STAGE === 'prod',
-      extra: {
-        ssl: process.env.STAGE === 'prod'
-              ? { rejectUnauthorized: false }
-              : null,
-      },
-
+    
   
   
   
