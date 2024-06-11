02d824c8f7be8bb3d10bda268abf64d2b7ea7ee4
Guido Salazar Vargas <Guido.salazar.vargas7@gmail.com>
2024-06-11 18:54:37 -0400

    Arreglando Deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,24 @@ import { PaymentsModule } from './payments/payments.module';
 
 
 @Module({
-  imports: [PaymentsModule],
+  imports: [
+    PaymentsModule,
+
+    ConfigModule.forRoot({isGlobal:true}),
+
+    TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl: process.env.STAGE === 'prod'
+              ? { rejectUnauthorized: false }
+              : null,
+      },
+
+  
+  
+  
+  
+  ],
 
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 
 async function bootstrap() {
   const logger = new Logger('Payments-ms');
+  
 
   const app = await NestFactory.create(AppModule, {
     rawBody: true
   });
-
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
